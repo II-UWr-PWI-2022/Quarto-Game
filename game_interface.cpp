9a05b9dae68189bcc1af02c5ae5a211e04038b05
Jakub Karwala <jakukarwala@gmail.com>
2023-01-17 22:52:28 +0100

    build: define add positions of fields that are next to the board

--- game_interface.cpp
+++ game_interface.cpp
@@ -16,9 +16,26 @@ class fieldOnBoard{
     2. b/s big or small
     3. c/s shape: circle or square
     4. h/s hollow-top or solid-top
-    if NULL then ther is no piece
+    if empty then ther is no piece
     */
-    string whatTypeOfPieceisOn=NULL;
+    string whatTypeOfPieceisOn="";
+};
+class fieldNextToBoard{
+    public:
+    bool isPieceStillOn=true;
+    int XpositionOfLeftUpCorner;
+    int YpositionOfLeftUpCorner;
+    int XpositionOfRightDownCorner;
+    int YpositionOfRightDownCorner;
+    /*Code for pawns
+    Contains 4 characters
+    1. d/l dark or light (color)
+    2. b/s big or small
+    3. c/s shape: circle or square
+    4. h/s hollow-top or solid-top
+    if empty then ther is no piece
+    */
+    string whatTypeOfPieceIsOn;
 };
 int main(){
     //defining variables
@@ -36,6 +53,113 @@ int main(){
     Sprite backgroundSprite;
     backgroundSprite.setTexture(backgroundTexture);
     backgroundSprite.setPosition(0,0);
+    //defining fields next to the board (pieces starting positions)
+    /*
+    l1              r1
+    l2              r2
+    l3              r3
+    l4              r4
+    l5              r5
+    l6              r6
+    l7              r7
+    l8              r8
+    */
+    fieldNextToBoard l1;
+    l1.XpositionOfLeftUpCorner=8;
+    l1.YpositionOfLeftUpCorner=16;
+    l1.XpositionOfRightDownCorner=61;
+    l1.YpositionOfRightDownCorner=69;
+    l1.whatTypeOfPieceIsOn="dbch";
+    fieldNextToBoard l2;
+    l2.XpositionOfLeftUpCorner=8;
+    l2.YpositionOfLeftUpCorner=93;
+    l2.XpositionOfRightDownCorner=61;
+    l2.YpositionOfRightDownCorner=142;
+    l2.whatTypeOfPieceIsOn="dbcs";
+    fieldNextToBoard l3;
+    l3.XpositionOfLeftUpCorner=8;
+    l3.YpositionOfLeftUpCorner=166;
+    l3.XpositionOfRightDownCorner=61;
+    l3.YpositionOfRightDownCorner=116;
+    l3.whatTypeOfPieceIsOn="dbsh";
+    fieldNextToBoard l4;
+    l4.XpositionOfLeftUpCorner=8;
+    l4.YpositionOfLeftUpCorner=239;
+    l4.XpositionOfRightDownCorner=61;
+    l4.YpositionOfRightDownCorner=291;
+    l4.whatTypeOfPieceIsOn="dbss";
+    fieldNextToBoard l5;
+    l5.XpositionOfLeftUpCorner=8;
+    l5.YpositionOfLeftUpCorner=319;
+    l5.XpositionOfRightDownCorner=61;
+    l5.YpositionOfRightDownCorner=363;
+    l5.whatTypeOfPieceIsOn="dsch";
+    fieldNextToBoard l6;
+    l6.XpositionOfLeftUpCorner=8;
+    l6.YpositionOfLeftUpCorner=391;
+    l6.XpositionOfRightDownCorner=61;
+    l6.YpositionOfRightDownCorner=435;
+    l6.whatTypeOfPieceIsOn="dscs";
+    fieldNextToBoard l7;
+    l7.XpositionOfLeftUpCorner=8;
+    l7.YpositionOfLeftUpCorner=465;
+    l7.XpositionOfRightDownCorner=61;
+    l7.YpositionOfRightDownCorner=510;
+    l7.whatTypeOfPieceIsOn="dssh";
+    fieldNextToBoard l8;
+    l8.XpositionOfLeftUpCorner=8;
+    l8.YpositionOfLeftUpCorner=539;
+    l8.XpositionOfRightDownCorner=61;
+    l8.YpositionOfRightDownCorner=588;
+    l8.whatTypeOfPieceIsOn="dsss";
+    fieldNextToBoard r1;
+    r1.XpositionOfLeftUpCorner=737;
+    r1.YpositionOfLeftUpCorner=14;
+    r1.XpositionOfRightDownCorner=790;
+    r1.YpositionOfRightDownCorner=60;
+    r1.whatTypeOfPieceIsOn="lbch";
+    fieldNextToBoard r2;
+    r2.XpositionOfLeftUpCorner=737;
+    r2.YpositionOfLeftUpCorner=88;
+    r2.XpositionOfRightDownCorner=790;
+    r2.YpositionOfRightDownCorner=139;
+    r2.whatTypeOfPieceIsOn="lbcs";
+    fieldNextToBoard r3;
+    r3.XpositionOfLeftUpCorner=737;
+    r3.YpositionOfLeftUpCorner=164;
+    r3.XpositionOfRightDownCorner=790;
+    r3.YpositionOfRightDownCorner=213;
+    r3.whatTypeOfPieceIsOn="lbsh";
+    fieldNextToBoard r4;
+    r4.XpositionOfLeftUpCorner=737;
+    r4.YpositionOfLeftUpCorner=236;
+    r4.XpositionOfRightDownCorner=790;
+    r4.YpositionOfRightDownCorner=286;
+    r4.whatTypeOfPieceIsOn="lbss";
+    fieldNextToBoard r5;
+    r5.XpositionOfLeftUpCorner=737;
+    r5.YpositionOfLeftUpCorner=313;
+    r5.XpositionOfRightDownCorner=790;
+    r5.YpositionOfRightDownCorner=360;
+    r5.whatTypeOfPieceIsOn="lsch";
+    fieldNextToBoard r6;
+    r6.XpositionOfLeftUpCorner=737;
+    r6.YpositionOfLeftUpCorner=385;
+    r6.XpositionOfRightDownCorner=790;
+    r6.YpositionOfRightDownCorner=435;
+    r6.whatTypeOfPieceIsOn="lscs";
+    fieldNextToBoard r7;
+    r7.XpositionOfLeftUpCorner=737;
+    r7.YpositionOfLeftUpCorner=459;
+    r7.XpositionOfRightDownCorner=790;
+    r7.YpositionOfRightDownCorner=506;
+    r7.whatTypeOfPieceIsOn="lssh";
+    fieldNextToBoard r8;
+    r8.XpositionOfLeftUpCorner=737;
+    r8.YpositionOfLeftUpCorner=535;
+    r8.XpositionOfRightDownCorner=790;
+    r8.YpositionOfRightDownCorner=582;
+    r8.whatTypeOfPieceIsOn="lsss";
     //defining fields on board
     /*
           a1
@@ -138,7 +262,7 @@ int main(){
         //Drawing to screen
         window.clear();
         //if game ends
-        if(endingConidtion==true){
+        /*if(endingConidtion==true){
             //if player won
             if(ifPlayerWon==true){
 
@@ -147,8 +271,7 @@ int main(){
             else{
 
             }
-        }
-
+        }*/
         window.draw(backgroundSprite);
         window.display();
     }
