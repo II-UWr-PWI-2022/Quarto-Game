c183885a4291b6370506badede5dabb07a3bcf96
Joanna <asinska.js@gmail.com>
2023-01-19 18:33:35 +0100

    build: add evaluating function to minmax bot

---
+++ src/Minmax_bot.cpp
@@ -0,0 +1,195 @@
+#include "minmax_bot.h"
+
+bool Bot::is_line_winning(char p1, char p2, char p3, char p4)
+{
+    if ((p1|p2|p3|p4)&(1<<4)) return false;
+    char mask = (1<<4)-1;
+    return (p1&p2&p3&p4&mask) || ((p1^mask)&(p2^mask)&(p3^mask)&(p4^mask)&mask);
+}
+
+
+bool Bot::is_board_winning()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (is_line_winning(board[i][0], board[i][1], board[i][2], board[i][3])) return true;
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        if (is_line_winning(board[0][i], board[1][i], board[2][i], board[3][i])) return true;
+    }
+    return (is_line_winning(board[0][0], board[1][1], board[2][2], board[3][3]) 
+        || is_line_winning(board[0][3], board[1][2], board[2][1], board[3][0]));
+}
+
+int Bot::evaluate(int piece)
+{
+    int value = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (board[i][j] == EMPTY)
+            {
+                board[i][j] = piece;
+                if (is_board_winning()) value = MAX_BOARD_VALUE;
+                board[i][j] = EMPTY;
+            }
+        }
+    }
+    if (value == MAX_BOARD_VALUE) return value;
+    
+    int count[4][2] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}};
+    for (int piece = 0; piece <16; piece++)
+    {
+        if (pieces[piece] == false) continue;
+        for (int i = 0; i < 4; i++)
+        {
+            count[i][((piece&(1<<i))>0)]++;
+        }
+    }
+
+    int mask = (1<<4)-1;
+    for (int i = 0; i < 4; i++)
+    {
+        int andv = mask, and_neg = mask;
+        for (int j = 0; j < 4; j++)
+        {
+            if (board[i][j] == EMPTY) continue;
+            andv &= board[i][j];
+            and_neg &= (board[i][j]^mask);
+        }
+        for (int j = 0; j < 4; j++)
+        {
+            if (andv&(1<<j)) value += count[j][1];
+            if (and_neg&(1<<j)) value += count[j][0];
+        }
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        int andv = mask, and_neg = mask;
+        for (int j = 0; j < 4; j++)
+        {
+            if (board[j][i] == EMPTY) continue;
+            andv &= board[j][i];
+            and_neg &= (board[j][i]^mask);
+        }
+        for (int j = 0; j < 4; j++)
+        {
+            if (andv&(1<<j)) value += count[j][1];
+            if (and_neg&(1<<j)) value += count[j][0];
+        }
+    }
+    int andv = mask, and_neg = mask;
+    for (int i = 0, j = 0; i < 4 && j < 4; i++, j++)
+    {
+        if (board[i][j] == EMPTY) continue;
+        andv &= board[i][j];
+        and_neg &= (board[i][j]^mask);
+    }
+    for (int j = 0; j < 4; j++)
+    {
+        if (andv&(1<<j)) value += count[j][1];
+        if (and_neg&(1<<j)) value += count[j][0];
+    }
+    andv = mask, and_neg = mask;
+    for (int i = 0, j = 3; i < 4 && j >= 0; i++, j--)
+    {
+        if (board[i][j] == EMPTY) continue;
+        andv &= board[i][j];
+        and_neg &= (board[i][j]^mask);
+    }
+    for (int j = 0; j < 4; j++)
+    {
+        if (andv&(1<<j)) value += count[j][1];
+        if (and_neg&(1<<j)) value += count[j][0];
+    }
+    return value;
+}
+
+int Bot::minmax(int depth, int piece, int max_depth)
+{
+    if(depth==max_depth)
+    {
+        if(depth&1)
+        {
+            return MAX_BOARD_VALUE-evaluate(piece);
+        } 
+        else 
+        {
+            return evaluate(piece);
+        }
+    }
+    int max_move=MIN_BOARD_VALUE;
+    int min_move=MAX_BOARD_VALUE;
+    int evaluated_move;
+    for(int row=0;row<4;row++)
+    {
+        for(int column=0;column<4;column++)
+        {
+            if(board[row][column]!=EMPTY) continue;
+            pieces[piece]=false;
+            board[row][column]=piece;
+            if(is_board_winning())
+            {
+                if(depth&1)
+                {
+                    return MIN_BOARD_VALUE;
+                } 
+                else 
+                {
+                    return MAX_BOARD_VALUE;
+                }
+            }
+            for(int opponent_piece=0;opponent_piece<16;piece++)
+            {
+                if(!pieces[piece]) continue;
+                evaluated_move=minmax(depth+1,opponent_piece,max_depth);
+                if(depth==0)
+                {
+                    if(max_move<evaluated_move)
+                    {
+                        max_move=evaluated_move;
+                        ans_piece=piece;
+                        ans_board_field.first=row;
+                        ans_board_field.second=column;
+                    }
+                    continue;
+                }
+                max_move=max(max_move,evaluated_move);
+                min_move=min(min_move,evaluated_move);
+            }
+            pieces[piece]=true;
+            board[row][column]=EMPTY;
+        }
+    }
+    if(depth&1)
+    {
+        return min_move;
+    } 
+    else 
+    {
+        return max_move;
+    }
+}
+
+
+int Bot::get_piece_type()
+{
+    return ans_piece;
+}
+
+pair<int,int> Bot::get_board_field(Quarto_game *game,int piece)
+{
+    for(int row=0;row<4;row++)
+    {
+        for(int column=0;column<4;column++)
+        {   
+            board[row][column] = game->get_piece_type_from_board_field(row,column);
+        }
+    }
+    minmax(0,piece,2);
+    pieces[piece]=false;
+    pieces[ans_piece]=false;
+    return ans_board_field;
+}
---
+++ src/Minmax_bot.h
@@ -0,0 +1,28 @@
+#ifndef EMPTY_BOT_H
+#define EMPTY_BOT_H
+
+#include <string>
+#include "Quarto_game.h"
+
+using namespace std;
+
+class Bot
+{
+    char board[4][4];
+    bool pieces[16] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    bool is_line_winning(int p1, int p2, int p3, int p4);
+    bool is_board_winning();
+    int evaluate(int piece);
+    int minmax(int depth,int piece,int max_depth);
+    pair<int,int> ans_board_field;
+    int ans_piece;
+    const int EMPTY=16;
+    const int MAX_BOARD_VALUE=650;
+    const int MIN_BOARD_VALUE=0;
+public:
+
+    int get_piece_type();
+    pair<int,int> get_board_field(Quarto_game *game,int piece);
+};
+
+#endif //EMPTY_BOT_H
