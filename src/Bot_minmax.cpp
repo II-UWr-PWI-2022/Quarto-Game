930b736813e9f8bd860e16c1894bbd76d0854857
Jan Burdzicki <janburdzicki@gmail.com>
2023-01-21 21:45:50 +0100

    refactor: refactor minmax bot

--- src/Bot_minmax.cpp
+++ src/Bot_minmax.cpp
@@ -2,180 +2,215 @@
 
 bool Bot_Minmax::is_line_winning(int p1, int p2, int p3, int p4)
 {
-    if ((p1|p2|p3|p4)&(1<<4)) return false;
-    int mask = (1<<4)-1;
-    return (p1&p2&p3&p4&mask) || ((p1^mask)&(p2^mask)&(p3^mask)&(p4^mask)&mask);
+	return (((p1 | p2 | p3 | p4) & EMPTY_FIELD == 0) && (p1 & p2 & p3 & p4) || (~(p1 | p2 | p3 | p4) & MASK));
 }
 
 bool Bot_Minmax::is_board_winning()
 {
-    for (int i = 0; i < 4; i++)
-    {
-        if (is_line_winning(board[i][0], board[i][1], board[i][2], board[i][3])) return true;
-    }
-    for (int i = 0; i < 4; i++)
-    {
-        if (is_line_winning(board[0][i], board[1][i], board[2][i], board[3][i])) return true;
-    }
-    return (is_line_winning(board[0][0], board[1][1], board[2][2], board[3][3])
-        || is_line_winning(board[0][3], board[1][2], board[2][1], board[3][0]));
+	for(int i = 0; i < MAX_N; i++)
+	{
+		if(is_line_winning(board[i][0], board[i][1], board[i][2], board[i][3])) return true;
+	}
+
+	for(int i = 0; i < MAX_N; i++)
+	{
+		if(is_line_winning(board[0][i], board[1][i], board[2][i], board[3][i])) return true;
+	}
+
+	return (is_line_winning(board[0][0], board[1][1], board[2][2], board[3][3])
+		|| is_line_winning(board[0][3], board[1][2], board[2][1], board[3][0]));
 }
 
 int Bot_Minmax::evaluate(int piece)
 {
-    int value = 0;
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            if (board[i][j] == EMPTY)
-            {
-                board[i][j] = piece;
-                if (is_board_winning()) value = MAX_BOARD_VALUE;
-                board[i][j] = EMPTY;
-            }
-        }
-    }
-    if (value == MAX_BOARD_VALUE) return value;
+	int value = 0;
 
-    int count[4][2] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}};
-    for (int piece = 0; piece <16; piece++)
-    {
-        if (pieces[piece] == false) continue;
-        for (int i = 0; i < 4; i++)
-        {
-            count[i][((piece&(1<<i))>0)]++;
-        }
-    }
+	for(int i = 0; i < MAX_N; i++)
+	{
+		for(int j = 0; j < MAX_N; j++)
+		{
+			if(board[i][j] == EMPTY_FIELD)
+			{
+				board[i][j] = piece;
 
-    int mask = (1<<4)-1;
-    for (int i = 0; i < 4; i++)
-    {
-        int andv = mask, and_neg = mask;
-        for (int j = 0; j < 4; j++)
-        {
-            if (board[i][j] == EMPTY) continue;
-            andv &= board[i][j];
-            and_neg &= (board[i][j]^mask);
-        }
-        for (int j = 0; j < 4; j++)
-        {
-            if (andv&(1<<j)) value += count[j][1];
-            if (and_neg&(1<<j)) value += count[j][0];
-        }
-    }
-    for (int i = 0; i < 4; i++)
-    {
-        int andv = mask, and_neg = mask;
-        for (int j = 0; j < 4; j++)
-        {
-            if (board[j][i] == EMPTY) continue;
-            andv &= board[j][i];
-            and_neg &= (board[j][i]^mask);
-        }
-        for (int j = 0; j < 4; j++)
-        {
-            if (andv&(1<<j)) value += count[j][1];
-            if (and_neg&(1<<j)) value += count[j][0];
-        }
-    }
-    int andv = mask, and_neg = mask;
-    for (int i = 0, j = 0; i < 4 && j < 4; i++, j++)
-    {
-        if (board[i][j] == EMPTY) continue;
-        andv &= board[i][j];
-        and_neg &= (board[i][j]^mask);
-    }
-    for (int j = 0; j < 4; j++)
-    {
-        if (andv&(1<<j)) value += count[j][1];
-        if (and_neg&(1<<j)) value += count[j][0];
-    }
-    andv = mask, and_neg = mask;
-    for (int i = 0, j = 3; i < 4 && j >= 0; i++, j--)
-    {
-        if (board[i][j] == EMPTY) continue;
-        andv &= board[i][j];
-        and_neg &= (board[i][j]^mask);
-    }
-    for (int j = 0; j < 4; j++)
-    {
-        if (andv&(1<<j)) value += count[j][1];
-        if (and_neg&(1<<j)) value += count[j][0];
-    }
-    return value;
+				if(is_board_winning()) value = MAX_BOARD_VALUE;
+
+				board[i][j] = EMPTY_FIELD;
+			}
+		}
+	}
+
+	if(value == MAX_BOARD_VALUE) return value;
+
+	int count[MAX_N][2] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}};
+
+	for(int piece = 0; piece < MAX_NUMBER_OF_PIECES; piece++)
+	{
+		if(pieces[piece] == false) continue;
+
+		for(int i = 0; i < MAX_N; i++)
+		{
+			count[i][((piece & (1 << i)) > 0)]++;
+		}
+	}
+
+	for(int i = 0; i < MAX_N; i++)
+	{
+		int andv = MASK, and_neg = MASK;
+
+		for(int j = 0; j < MAX_N; j++)
+		{
+			if(board[i][j] == EMPTY_FIELD) continue;
+
+			andv &= board[i][j];
+			and_neg &= (board[i][j] ^ MASK);
+		}
+
+		for(int j = 0; j < MAX_N; j++)
+		{
+			if(andv & (1 << j)) value += count[j][1];
+			if(and_neg & (1 << j)) value += count[j][0];
+		}
+	}
+
+	for(int i = 0; i < MAX_N; i++)
+	{
+		int andv = MASK, and_neg = MASK;
+
+		for(int j = 0; j < MAX_N; j++)
+		{
+			if(board[j][i] == EMPTY_FIELD) continue;
+
+			andv &= board[j][i];
+			and_neg &= (board[j][i] ^ MASK);
+		}
+
+		for(int j = 0; j < MAX_N; j++)
+		{
+			if(andv & (1 << j)) value += count[j][1];
+			if(and_neg & (1 << j)) value += count[j][0];
+		}
+	}
+
+	int andv = MASK, and_neg = MASK;
+
+	for(int i = 0, j = 0; i < MAX_N && j < MAX_N; i++, j++)
+	{
+		if(board[i][j] == EMPTY_FIELD) continue;
+
+		andv &= board[i][j];
+		and_neg &= (board[i][j]^MASK);
+	}
+
+	for(int i = 0; i < MAX_N; i++)
+	{
+		if(andv & (1 << i)) value += count[i][1];
+		if(and_neg & (1 << i)) value += count[i][0];
+	}
+
+	andv = MASK, and_neg = MASK;
+
+	for(int i = 0, j = MAX_N - 1; i < MAX_N && j >= 0; i++, j--)
+	{
+		if(board[i][j] == EMPTY_FIELD) continue;
+
+		andv &= board[i][j];
+		and_neg &= (board[i][j] ^ MASK);
+	}
+
+	for(int i = 0; i < MAX_N; i++)
+	{
+		if(andv & (1 << i)) value += count[i][1];
+		if(and_neg & (1 << i)) value += count[i][0];
+	}
+
+	return value;
 }
 
 int Bot_Minmax::minmax(int depth, int piece, int max_depth)
 {
-    if(depth==max_depth)
-    {
-        if(depth&1)
-        {
-            return MAX_BOARD_VALUE-evaluate(piece);
-        }
-        else
-        {
-            return evaluate(piece);
-        }
-    }
-    int max_move=MIN_BOARD_VALUE;
-    int min_move=MAX_BOARD_VALUE;
-    int evaluated_move;
-    for(int row=0;row<4;row++)
-    {
-        for(int column=0;column<4;column++)
-        {
-            if(board[row][column]!=EMPTY) continue;
-            pieces[piece]=false;
-            board[row][column]=piece;
-            if(is_board_winning())
-            {
-                if(depth&1)
-                {
-                    return MIN_BOARD_VALUE;
-                }
-                else
-                {
-                    return MAX_BOARD_VALUE;
-                }
-            }
-            for(int opponent_piece=0;opponent_piece<16;piece++)
-            {
-                if(!pieces[piece]) continue;
-                evaluated_move=minmax(depth+1,opponent_piece,max_depth);
-                if(depth==0)
-                {
-                    if(max_move<evaluated_move)
-                    {
-                        max_move=evaluated_move;
-                        chosen_piece=piece;
-                        chosen_board_field.first=row;
-                        chosen_board_field.second=column;
-                    }
-                    continue;
-                }
-                max_move=max(max_move,evaluated_move);
-                min_move=min(min_move,evaluated_move);
-            }
-            pieces[piece]=true;
-            board[row][column]=EMPTY;
-        }
-    }
-    if(depth&1)
-    {
-        return min_move;
-    }
-    else
-    {
-        return max_move;
-    }
+	if(depth == max_depth)
+	{
+		if(depth & 1)
+		{
+			return MAX_BOARD_VALUE - evaluate(piece);
+		}
+
+		else
+		{
+			return evaluate(piece);
+		}
+	}
+
+	int max_move = MIN_BOARD_VALUE;
+	int min_move = MAX_BOARD_VALUE;
+	int evaluated_move;
+
+	for(int row = 0; row < MAX_N; row++)
+	{
+		for(int column = 0; column < MAX_N; column++)
+		{
+			if(board[row][column] != EMPTY_FIELD) continue;
+
+			pieces[piece] = false;
+			board[row][column] = piece;
+
+			if(is_board_winning())
+			{
+				if(depth & 1)
+				{
+					return MIN_BOARD_VALUE;
+				}
+
+				else
+				{
+					return MAX_BOARD_VALUE;
+				}
+			}
+
+			for(int opponent_piece = 0; opponent_piece < MAX_NUMBER_OF_PIECES; piece++)
+			{
+				if(!pieces[piece]) continue;
+
+				evaluated_move = minmax(depth + 1, opponent_piece, max_depth);
+
+				if(depth == 0)
+				{
+					if(max_move < evaluated_move)
+					{
+						max_move = evaluated_move;
+						chosen_piece = piece;
+						chosen_board_field.first = row;
+						chosen_board_field.second = column;
+					}
+
+					continue;
+				}
+
+				max_move = max(max_move, evaluated_move);
+				min_move = min(min_move, evaluated_move);
+			}
+
+			pieces[piece] = true;
+			board[row][column] = EMPTY_FIELD;
+		}
+	}
+
+	if(depth & 1)
+	{
+		return min_move;
+	}
+
+	else
+	{
+		return max_move;
+	}
 }
 
 
 int Bot_Minmax::get_chosen_piece_type()
 {
-    return chosen_piece;
+	return chosen_piece;
 }
 
 pair <int, int> Bot_Minmax::get_chosen_board_field()
@@ -185,16 +220,17 @@ pair <int, int> Bot_Minmax::get_chosen_board_field()
 
 void Bot_Minmax::analyze_position(Quarto_game* game, int piece)
 {
-    vector <vector <int>> board = game->get_board();
+	vector <vector <int>> board = game->get_board();
 
-    vector <bool> pieces = vector <bool> (MAX_NUMBER_OF_PIECES, true);
+	vector <bool> pieces = vector <bool> (MAX_NUMBER_OF_PIECES, true);
 
 	for(int i = 0; i < MAX_NUMBER_OF_PIECES; i++)
 	{
 		pieces[i] = !game->is_piece_used(i);
 	}
 
-    minmax(0, piece, 2);
-    pieces[piece] = false;
-    pieces[chosen_piece] = false;
+	minmax(0, piece, 2);
+
+	pieces[piece] = false;
+	pieces[chosen_piece] = false;
 }
