64bc3bf45e5cb13a1a69516992dc9e1f371c3146
Krzysztof Szymański <kszymanski926@gmail.com>
2023-01-22 20:31:24 +0100

    chore: change bot ids from A,B,C to 1,2,3

--- competition/comp.sh
+++ competition/comp.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 num_of_games=1
-bot_A_pts=0
-bot_B_pts=0
-bot_C_pts=0
+bot_1_pts=0
+bot_2_pts=0
+bot_3_pts=0
 
-# simulate competition between bots A and B
+# bots 1 and 2, easy mode
 for i in {1..$num_of_games}
 do
-	../build/main < easy_AB > out.out &
+	../build/main < easy12 > out.out &
 	ID=$!
 	kill $ID
 	if grep "Player A wins" out.out; then
-		(( bot_A_pts++ ))
+		(( bot_1_pts++ ))
 	else
-		(( bot_B_pts++ ))
+		(( bot_2_pts++ ))
 	fi
 done
 
