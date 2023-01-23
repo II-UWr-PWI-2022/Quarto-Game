23b0d6518b10f71b430dd5caceecf5cb8ae7e8e1
Krzysztof Szymański <kszymanski926@gmail.com>
2023-01-24 00:38:33 +0100

    fix: added draw condition to script

--- competition/comp.sh
+++ competition/comp.sh
@@ -5,79 +5,85 @@ bot_2_pts=0
 bot_3_pts=0
 
 # bots 1 and 2, easy mode
-for i in {1..5}
+for i in {1..10}
 do
 	../build/main < easy12 > out.out &
 	ID=$!
 	kill $ID
 	if grep "Player A wins" out.out; then
 		(( bot_1_pts++ ))
-	else
+	fi
+	if grep "Player B wins" out.out; then
 		(( bot_2_pts++ ))
 	fi
 done
 
 # bots 1 and 2, hard mode
-for i in {1..5}
+for i in {1..10}
 do
 	../build/main < hard12 > out.out &
 	ID=$!
 	kill $ID
 	if grep "Player A wins" out.out; then
 		(( bot_1_pts += 3 ))
-	else
+	fi
+	if grep "Player B wins" out.out; then
 		(( bot_2_pts += 3 ))
 	fi
 done
 
 # bots 1 and 3, easy mode
-for i in {1..5}
+for i in {1..10}
 do
 	../build/main < easy13 > out.out &
 	ID=$!
 	kill $ID
 	if grep "Player A wins" out.out; then
 		(( bot_1_pts++ ))
-	else
+	fi
+	if grep "Player B wins" out.out; then
 		(( bot_3_pts++ ))
 	fi
 done
 
 # bots 1 and 3, hard mode
-for i in {1..5}
+for i in {1..10}
 do
 	../build/main < hard13 > out.out &
 	ID=$!
 	kill $ID
 	if grep "Player A wins" out.out; then
 		(( bot_1_pts += 3 ))
-	else
+	fi
+	if grep "Player B wins" out.out; then
 		(( bot_3_pts += 3 ))
 	fi
 done
 
 # bots 2 and 3, easy mode
-for i in {1..5}
+for i in {1..10}
 do
 	../build/main < easy23 > out.out &
 	ID=$!
 	kill $ID
 	if grep "Player A wins" out.out; then
 		(( bot_2_pts++ ))
-	else
+	fi
+	if grep "Player B wins" out.out; then
 		(( bot_3_pts++ ))
 	fi
 done
 
 # bots 2 and 3, hard mode
-for i in {1..5}
+for i in {1..10}
 do
 	../build/main < hard23 > out.out &
 	ID=$!
 	kill $ID
 	if grep "Player A wins" out.out; then
 		(( bot_2_pts += 3 ))
-	else
+	fi
+	if grep "Player A wins" out.out; then
 		(( bot_3_pts += 3 ))
 	fi
 done
